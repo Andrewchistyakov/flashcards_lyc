b71c0a3d6c7cb08333a94a710ee988e54d873436
andrew <chistyakgg@gmail.com>
2025-02-10 22:25:33 +0300

    adding cards with ID now

--- flashcard.cpp
+++ flashcard.cpp
@@ -1,18 +1,54 @@
 #include "flashcard.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
+
+// функция получает следующий ID
+int getNextID(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        // если файла нету берем айдишник 1
+        return 1;
+    }
+
+    std::string line, lastLine;
+    while (std::getline(file, line)) {
+        lastLine = line; // получем последнюю строку 
+    }
+    file.close();
+
+    if (lastLine.empty()) {
+        return 1; // если файлик пустой берем айди 1
+    }
+
+    // получаем айди из последней строки
+    std::istringstream iss(lastLine);
+    std::string idStr;
+    std::getline(iss, idStr, ','); // читаем до первой запятой
+    try {
+        return std::stoi(idStr) + 1; // конвертируем полученную строку в инт и прибавляем 1
+    } catch (...) {
+        return -1; // если чето не так возвращаем -1 как знак ошибки
+    }
+}
 
 // функция записывает карточку в .csv файл
 bool addFlashcard(const std::string& front, const std::string& back) {
+
+    const std::string filename = "flashcards.csv";
+
+    // считаем какой айди следующий 
+    int id = getNextID(filename);
+
     // открываем файл в режиме добавления
-    std::ofstream file("flashcards.csv", std::ios::app);
+    std::ofstream file(filename, std::ios::app);
     if (!file.is_open()) {
         std::cerr << "Error: Could not open flashcards.csv for writing." << std::endl;
         return false;
     }
 
     // записываем в файл карточку
-    file << "\"" << front << "\",\"" << back << "\"" << std::endl;
+    file << id <<",\"" << front << "\",\"" << back << "\"" << std::endl;
 
     // закрываем файл
     file.close();
