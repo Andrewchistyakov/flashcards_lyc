baf799b58a8f18dc827afb8d4a31f738be336203
andrew <chistyakgg@gmail.com>
2025-02-10 19:09:39 +0300

    added comments

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 
 int main(int, char* argv[])
 {
+    // cmdl - парсер из "Argh!", из него получаем аргументы
     argh::parser cmdl(argv);
 
+    // тестовая команда привет, по ней можно будет проверять, запускается ли вообще программа
     if (cmdl[1] == "privet") {
         std::cout << "Hello world!\n";
     } else {
